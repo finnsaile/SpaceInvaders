e4ba8fca3b1e5e03764406630dd4d5febed1a917
Soulsaver35815 <86435297+finnsaile@users.noreply.github.com>
2021-08-20 04:59:39 +0200

    Changed function and variable names. Removed unused variables.

--- headers/CPlayer.h
+++ headers/CPlayer.h
@@ -10,27 +10,23 @@ class CPlayer : public sf::Drawable, public sf::Transformable
 {
     public:
         CPlayer(sf::Vector2f& scaleVec);
-        ~CPlayer();
+        virtual ~CPlayer();
         void playerTick();
         void setState(bool W, bool A, bool S, bool D);
 
     private:
-        long long bigcounter = 0;
-        long long bigc = 0;
         double m_accumulator = 0;
         State m_state = REST;
         sf::Sprite m_player_sprite;
         sf::Texture m_player_texture;
         sf::Clock m_clock;
         sf::Vector2f& m_scale;
-        sf::Vector2f m_current_pos;
-        sf::Vector2f m_previous_pos;
-        std::vector<State> m_vecX;
-        std::vector<State> m_vecY;
+        std::vector<State> m_stack_x;
+        std::vector<State> m_stack_y;
 
         void initPlayerModel();
         sf::Vector2f calcMovement(float delta_time);   
-        void handleVector(bool& boolIn, std::vector<State>& vecIn, State stateIn);
+        void handleStateStack(bool& boolIn, std::vector<State>& vecIn, State stateIn);
         virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
 };
 
